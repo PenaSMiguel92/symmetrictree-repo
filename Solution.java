3c1e36bf613bf70eda89297561cbce732208fdf6
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-23 17:40:22 -0700

    solution found

--- Solution.java
+++ Solution.java
@@ -1,3 +1,16 @@
 public class Solution {
-public boolean isSymmetric(TreeNode root) {
-}} 
+    public boolean isSymmetric(TreeNode root) {
+        return checkSymmetry(root, root);
+    }
+    public boolean checkSymmetry(TreeNode left, TreeNode right) {
+        if (left == null && right == null)
+            return true;
+        
+        if (left != null && right != null && left.val == right.val) {
+            boolean bool1 = checkSymmetry(left.left, right.right);
+            boolean bool2 = checkSymmetry(left.right, right.left);
+            return bool1 && bool2;
+        }
+        return false;
+    }
+} 
